997c8c0f40e1175497324d1ac616d6860979ac0c
DevSteven1 <stevenpajarodev@gmail.com>
2024-04-27 12:15:46 -0500

    Db_Connection_Fail_Page:2

--- assets/Conexion.php
+++ assets/Conexion.php
@@ -4,9 +4,10 @@ $USER = "root";
 $PASSWORD = "";
 $DATA_BASE = "restaurantephp";
 
-$CONEXION = new mysqli($SERVER,$USER,$PASSWORD,$DATA_BASE);
-
-if($CONEXION -> connect_error){
+try {
+    $CONEXION = new mysqli($SERVER, $USER, $PASSWORD, $DATA_BASE);
+} catch (mysqli_sql_exception $e) {
     header("Location: /Restaurante/pages/errors/ErrorConexionDb.php");
+    exit();
 }
 ?>
